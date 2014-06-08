beec8cc6dc69fdd0690c098b53be573fffccdade
Serge Gebhardt <serge.gebhardt@gmail.com>
2014-06-08 01:11:46 -0300

    Snap CI: add GOROOT

--- scripts/env.sh
+++ scripts/env.sh
@@ -3,5 +3,6 @@
 # This script is to be *sourced* by the other scripts to set up env variables
 
 export GO_DIST_DIR=$HOME/go_dist
+export GOROOT=$GO_DIST_DIR/go/
 export GOPATH=$HOME/go
-export PATH=$GOPATH/bin:$GO_DIST_DIR/go/bin:$PATH
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
