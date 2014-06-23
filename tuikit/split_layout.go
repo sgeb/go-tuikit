d2763cc0209ec008e6d0a25f215f8dfb712881f3
Serge Gebhardt <serge.gebhardt@gmail.com>
2014-06-23 16:51:07 +0200

    SplitLayout now supports splitting with a ratio

--- tuikit/split_layout.go
+++ tuikit/split_layout.go
@@ -4,6 +4,7 @@ type SplitLayout struct {
 	*BaseView
 
 	split1, split2 Painter
+	ratio          float32
 }
 
 func NewSplitLayout(split1, split2 Painter) *SplitLayout {
@@ -12,13 +13,24 @@ func NewSplitLayout(split1, split2 Painter) *SplitLayout {
 		split1:   split1,
 		split2:   split2,
 	}
+	l.SetRatio(0.5)
 	l.SetUpdateChildrenRect(l.updateChildrenRect)
 	return l
 }
 
+func (l *SplitLayout) SetRatio(ratio float32) {
+	switch {
+	case ratio > 1:
+		ratio = 1
+	case ratio < 0:
+		ratio = 0
+	}
+	l.ratio = ratio
+}
+
 func (l *SplitLayout) updateChildrenRect(rect Rect) error {
 	r1 := rect
-	r1.Height = rect.Height / 2
+	r1.Height = int(float32(rect.Height) * l.ratio)
 	l.AttachChild(l.split1, r1)
 
 	r2 := rect
